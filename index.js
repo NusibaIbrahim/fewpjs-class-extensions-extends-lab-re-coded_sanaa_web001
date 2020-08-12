8b3e27416751c178114ab06efb6024d8b1fe136d
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-12 16:50:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,26 +40,4 @@ triangle = new Triangle([5, 5, 5]);
 let triangle2 = new Triangle([15, 10, 1]);
 triangle.countSides;
 triangle.isValid;
-triangle2.isValid;let circle;
-let triangle;
-let square;
-
-class Polygon {
-  constructor(array) {
-    this.array = array;
-  }
-
-  get countSides() {
-    return this.array.length;
-  }
-  get perimeter() {
-    let sum = 0;
-    for (let index = 0; index < this.array.length; index++) {
-      sum += this.array[index];
-    }
-    return sum;
-  }
-}
-let polygon = new Polygon([5, 5, 5]);
-console.log(polygon.countSides);
-console.log(polygon.perimeter);
+triangle2.isValid;
